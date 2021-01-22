6258be0444c55264a372fc4541bc285e8c1e803d
Eric Kehoe <ekehoe32@gmail.com>
2021-01-22 15:57:11 -0700

    updated docs

--- docsrc/gen_docs.sh
+++ docsrc/gen_docs.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 eval "$(conda shell.bash hook)"
 conda activate DataSci
+
+global() {
+  shopt -s globstar
+  origdir="../datasci/"
+  for i in **/; do
+    sphinx-apidoc -o rst
+  done
+}
+
 sphinx-apidoc -o rst ../datasci/*
 make github
