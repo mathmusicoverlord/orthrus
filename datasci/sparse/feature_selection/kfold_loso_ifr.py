284624f4c78d54ee1d5c324c1ca6bea11c67fbac
ekeho <ekehoe32@gmail.com>
2021-03-03 14:05:54 -0700

    - added kfold loso ifr method

--- datasci/sparse/feature_selection/kfold_loso_ifr.py
+++ datasci/sparse/feature_selection/kfold_loso_ifr.py
@@ -6,40 +6,37 @@ subject out (LOSO) partitions.
 # imports
 from sklearn.base import BaseEstimator
 from sklearn.model_selection import StratifiedKFold
-from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.model_selection import LeaveOneOut
 import numpy as np
 import pandas as pd
 from sklearn.cluster import KMeans
 
 
-class KFSIFR(BaseEstimator):
+class KFLIFR(BaseEstimator):
 
     def __init__(self,
-                 threshold: float,
                  scorer: object,
                  classifier: object,
                  weights_handle: str,
                  n_splits_kfold: int,
-                 n_splits_shuffle: int,
-                 test_size_shuffle: float = .25,
                  random_state_kfold=None,
-                 random_state_shuffle=None,
+                 gamma: float = .01,
                  n_top_features: int = None,
                  jump_value: float = None,
                  kfold_labels=None):
 
         # set parameters
-        self.threshold = threshold
         self.scorer = scorer
         self.classifier = classifier
         self.kfold = StratifiedKFold(n_splits=n_splits_kfold, shuffle=True, random_state=random_state_kfold)
-        self.shuffle = StratifiedShuffleSplit(n_splits=n_splits_shuffle, test_size=test_size_shuffle, random_state=random_state_shuffle)
         self.weights_handle = weights_handle
+        self.gamma = gamma
         self.n_top_features = n_top_features
         self.jump_value = jump_value
         self.kfold_labels = kfold_labels
 
         # set attributes
+        self.loso = LeaveOneOut()
         self.results_ = pd.DataFrame()
 
     def fit(self, X, y):
@@ -61,12 +58,12 @@ class KFSIFR(BaseEstimator):
             Xi_test = X[kf_test_index, :]; yi_test = y[kf_test_index]
 
             # intialize feature set
-            Si = pd.DataFrame(index=St, columns=np.arange(self.shuffle.n_splits)).fillna(False).astype(bool)
+            Si = pd.DataFrame(index=St, columns=np.arange(Xi_train.shape[0])).fillna(False).astype(bool)
 
-            for j, (shuffle_train_index, shuffle_test_index) in enumerate(self.shuffle.split(Xi_train, yi_train)):
-                print("Starting shuffle split " + str(j) + "...")
-                Xij_train = Xi_train[shuffle_train_index, :]; yij_train = yi_train[shuffle_train_index]
-                Xij_valid = Xi_train[shuffle_test_index, :]; yij_valid = yi_train[shuffle_test_index]
+            for j, (loso_train_index, loso_test_index) in enumerate(self.loso.split(Xi_train, yi_train)):
+                print("Starting LOSO split " + str(j) + "...")
+                Xij_train = Xi_train[loso_train_index, :]; yij_train = yi_train[loso_train_index]
+                Xij_valid = Xi_train[loso_test_index, :]; yij_valid = yi_train[loso_test_index]
                 Sij = [] # intial feature set
                 while True:
                     print("Starting IFR pass...")
@@ -85,22 +82,25 @@ class KFSIFR(BaseEstimator):
                         f_ratios = f_weights_sorted[:-1]/f_weights_sorted[1:]
                         f_ratios[np.isinf(f_ratios)] = 0
                         f_ratios[np.isnan(f_ratios)] = 0
-                        id = np.where(f_ratios > self.jump_value)[0][0]
-                        S = S[:id]
-                        S = Sc[S]
+                        try:
+                            id = np.where(f_ratios > self.jump_value)[0][0]
+                            S = S[:id]
+                            S = Sc[S]
+                        except IndexError:
+                            print("Jump failed, no features selected...")
+                            break
                     else:
                         S = Sc[S[:self.n_top_features]]
 
                     # record accuracy on validation set
                     #self.classifier.fit(Xij_train[:, S], yij_train)
-                    yij_pred = self.classifier.predict(Xij_valid[:, Sc])
-                    score = self.scorer(yij_valid, yij_pred)
-                    print("acc. score = " + str(score))
-
+                    #yij_pred = self.classifier.predict(Xij_valid[:, Sc])
+                    #score = self.scorer(yij_valid, yij_pred)
+                    #print("acc. score = " + str(score))
+                    Sij = np.array(list(set(Sij).union(set(S))))
+                    print("Feature set size = " + str(Sij.size))
                     # check threshold condition
-                    if score > self.threshold:
-                        Sij = np.array(list(set(Sij).union(set(S))))
-                    else:
+                    if Sij.size > self.gamma*St.size:
                         break
 
                 # set selected features
@@ -109,9 +109,3 @@ class KFSIFR(BaseEstimator):
             # compute frequencies and store into results
             Si = Si.sum(axis=1)
             self.results_[i] = Si
-
-
-
-
-
-
