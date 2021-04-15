efcec1f04f216cf5c3007b3ade63486ed8984508
Kartikay Sharma <sharmas.kartik@gmail.com>
2021-04-15 12:47:02 -0600

    Added partitioning module, this will be the place to define the partitioners. Implemented a train-test partitioner

---
+++ datasci/model_selection/partitioning.py
@@ -0,0 +1,11 @@
+'''This module defines various partitioning classes which partitions samples in batches of training and test split. 
+These classes must have the sklearn equivalent of a split method. The split method returns a list of train-test partitions; 
+one for each fold in the experiment. See sklearn.model_selection.KFold for an example partitioner.'''
+
+class TrainTestPartitioner():
+    def __init__(self, train_idx, test_idx):
+        self.train_idx = train_idx
+        self.test_idx = test_idx
+
+    def split(self, X, y):
+        return [[self.train_idx, self.test_idx]]
