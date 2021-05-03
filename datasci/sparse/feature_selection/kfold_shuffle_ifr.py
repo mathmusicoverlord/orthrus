5911184854e485d89f86076caca7c8a460b91aec
ekeho <ekehoe32@gmail.com>
2021-05-03 15:55:19 -0600

    - Updated shuffle ifr

--- datasci/sparse/feature_selection/kfold_shuffle_ifr.py
+++ datasci/sparse/feature_selection/kfold_shuffle_ifr.py
@@ -11,7 +11,6 @@ from sklearn.model_selection import LeaveOneOut
 from sklearn.model_selection import ShuffleSplit
 from sklearn.model_selection import GroupShuffleSplit
 from sklearn.metrics import balanced_accuracy_score as bsr
-from datasci.model_selection.partitioning import TrainPartitioner
 import numpy as np
 import pandas as pd
 from pandas import DataFrame
@@ -39,9 +38,6 @@ class KFSIFR(BaseEstimator):
 
         gamma (float): Classification rate used to break the IFR loop. The IFR loop will stop once the number of
             features extracted reaches this proportion of the total features.
-
-        max_feature_threshold (int): The maximum number of features that may be removed on an iteration of IFR.
-            Default is None.
 
         n_splits_shuffle (int): The number of shuffle splits to use for the inner loop. Default is 100.
 
@@ -73,8 +69,7 @@ class KFSIFR(BaseEstimator):
                  n_splits_kfold: int,
                  random_state_kfold=None,
                  train_test_splits=None,
-                 gamma: float = .6,
-                 max_feature_threshold=None,
+                 gamma: float = .01,
                  n_splits_shuffle = 100,
                  random_state_shuffle = None,
                  train_prop_shuffle = .8,
@@ -85,13 +80,9 @@ class KFSIFR(BaseEstimator):
 
         # set parameters
         self.classifier = classifier
-        if n_splits_kfold != 1:
-            self.kfold = StratifiedKFold(n_splits=n_splits_kfold, shuffle=True, random_state=random_state_kfold)
-        else:
-            self.kfold = TrainPartitioner()
+        self.kfold = StratifiedKFold(n_splits=n_splits_kfold, shuffle=True, random_state=random_state_kfold)
         self.weights_handle = weights_handle
         self.gamma = gamma
-        self.max_feature_threshold = max_feature_threshold
         self.n_top_features = n_top_features
         self.jump_ratio = jump_ratio
         self.train_test_splits = train_test_splits
@@ -128,12 +119,6 @@ class KFSIFR(BaseEstimator):
         # intialize results
         self.results_ = pd.DataFrame(index=St)
 
-        # set default value of maximum feature threshold
-        if self.max_feature_threshold is None:
-            max_feature_threshold = X.shape[1]
-        else:
-            max_feature_threshold = self.max_feature_threshold
-
         # loop through cv folds
         if self.train_test_splits is None:
             splits = self.kfold.split(X, y)
@@ -153,7 +138,6 @@ class KFSIFR(BaseEstimator):
             Si = pd.DataFrame(index=St, columns=np.arange(Xi_train.shape[0])).fillna(False).astype(bool)
 
             if groups is None:
-                groups_train = np.array(kf_train_index)
                 shuffle_splits = ShuffleSplit(n_splits=self.n_splits_shuffle,
                                               random_state=self.random_state_shuffle,
                                               train_size=self.train_prop_shuffle).split(Xi_train, yi_train)
@@ -187,12 +171,8 @@ class KFSIFR(BaseEstimator):
                         f_ratios = np.divide(a, b, out=np.zeros_like(a), where=b!=0)
                         try:
                             id = np.where(f_ratios > self.jump_ratio)[0][0]
-                            if (id + 1) < max_feature_threshold:
-                                S = S[:(id+1)]
-                                S = Sc[S]
-                            else:
-                                print("Maximum feature threshold exceeded, no features selected...")
-                                break
+                            S = S[:id]
+                            S = Sc[S]
                         except IndexError:
                             print("Jump failed, no features selected...")
                             break
@@ -209,7 +189,7 @@ class KFSIFR(BaseEstimator):
                     print("Feature set size = " + str(Sij.size))
 
                     # check threshold condition
-                    if score < self.gamma:
+                    if bsr < self.gamma:
                         break
 
                 # set selected features
--- scripts/visualize.py
+++ scripts/visualize.py
@@ -41,26 +41,15 @@ if __name__ == '__main__':
     from umap import UMAP
     from datasci.manifold.mds import MDS
     from datasci.core.helper import module_from_path
-    from datasci.core.helper import default_val
     import pandas as pd
 
     # set experiment parameters
     exp_params = module_from_path('exp_params', args.exp_params)
-    script_args = exp_params.VISUALIZE_ARGS
-
-    ## required script params
-    fig_dir = script_args.get('FIG_DIR', exp_params.FIG_DIR)
-    exp_name = script_args.get('EXP_NAME', exp_params.EXP_NAME)
-    class_attr = script_args.get('CLASS_ATTR', exp_params.CLASS_ATTR)
-    ds = script_args.get('DATASET', exp_params.DATASET)
-
-    ## optional script params
-    sample_ids = script_args.get('SAMPLE_IDS', default_val(exp_params, 'SAMPLE_IDS')),
-    feature_ids = script_args.get('FEATURE_IDS', default_val(exp_params, 'FEATURE_IDS'))
-    cross_attr = script_args.get('CROSS_ATTR', default_val(exp_params, 'CROSS_ATTR'))
-    save_name = script_args.get('SAVE_NAME', default_val(exp_params, 'VISUALIZE_SAVE_NAME'))
-    title = script_args.get('TITLE', default_val(exp_params, 'VISUALIZE_TITLE', val=''))
-    subtitle = script_args.get('SUBTITLE', default_val(exp_params, 'VISUALIZE_SUBTITLE', val=''))
+    exp_name = exp_params.EXP_NAME
+    class_attr = exp_params.CLASS_ATTR
+    ds = exp_params.DATASET
+    sample_ids = exp_params.SAMPLE_IDS
+    feature_ids = exp_params.FEATURE_IDS
 
     # grab dimension
     if args.dim == 1:
@@ -85,33 +74,18 @@ if __name__ == '__main__':
             palette = 'viridis'
         else:
             palette = 'bright'
-        if cross_attr is None:
-            backend_args = dict(palette=palette, alpha=.7, mrkr_list=['o'], s=200)
-        else:
-            backend_args = dict(palette=palette, alpha=.7, s=200)
+        backend_args = dict(palette=palette, alpha=.7, mrkr_list=['o'], s=200)
     elif args.backend == 'plotly':
         backend = 'plotly'
         backend_args = dict(figsize=(1500, 1000))
-
-    # set save name
-    if save_name is None:
-        if cross_attr is None:
-            save_name = '_'.join([ds.name, exp_name, embedding.__str__().split('(')[0].lower(), class_attr.lower()])
-        else:
-            save_name = '_'.join([ds.name, exp_name, embedding.__str__().split('(')[0].lower(), class_attr.lower(), cross_attr.lower()])
-
-    # set figure directory just in case
-    ds.path = fig_dir
 
     # visualize data
     ds.visualize(embedding=embedding,
                  sample_ids=sample_ids,
                  feature_ids=feature_ids,
                  attr=class_attr,
-                 cross_attr=cross_attr,
                  backend=backend,
-                 subtitle=subtitle, # <--- default show normalization and imputation methods used
-                 title=title,
+                 subtitle='', # <--- default show normalization and imputation methods used
                  save=True,
-                 save_name=save_name,
+                 save_name='_'.join([ds.name, exp_name, embedding.__str__().split('(')[0].lower(), class_attr.lower()]),
                  **backend_args)
