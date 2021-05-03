6684ef1159dd0a4a1f2ad94e8fe198fddc8fb040
ekeho <ekehoe32@gmail.com>
2021-05-03 15:55:19 -0600

    - Updated shuffle ifr

--- datasci/sparse/feature_selection/kfold_shuffle_ifr.py
+++ datasci/sparse/feature_selection/kfold_shuffle_ifr.py
@@ -39,9 +39,6 @@ class KFSIFR(BaseEstimator):
         gamma (float): Classification rate used to break the IFR loop. The IFR loop will stop once the number of
             features extracted reaches this proportion of the total features.
 
-        max_feature_threshold (int): The maximum number of features that may be removed on an iteration of IFR.
-            Default is None.
-
         n_splits_shuffle (int): The number of shuffle splits to use for the inner loop. Default is 100.
 
         random_state_shuffle (int): Random seed for shuffle splits.
@@ -72,8 +69,7 @@ class KFSIFR(BaseEstimator):
                  n_splits_kfold: int,
                  random_state_kfold=None,
                  train_test_splits=None,
-                 gamma: float = .6,
-                 max_feature_threshold=None,
+                 gamma: float = .01,
                  n_splits_shuffle = 100,
                  random_state_shuffle = None,
                  train_prop_shuffle = .8,
@@ -87,7 +83,6 @@ class KFSIFR(BaseEstimator):
         self.kfold = StratifiedKFold(n_splits=n_splits_kfold, shuffle=True, random_state=random_state_kfold)
         self.weights_handle = weights_handle
         self.gamma = gamma
-        self.max_feature_threshold = max_feature_threshold
         self.n_top_features = n_top_features
         self.jump_ratio = jump_ratio
         self.train_test_splits = train_test_splits
@@ -123,12 +118,6 @@ class KFSIFR(BaseEstimator):
 
         # intialize results
         self.results_ = pd.DataFrame(index=St)
-
-        # set default value of maximum feature threshold
-        if self.max_feature_threshold is None:
-            max_feature_threshold = X.shape[1]
-        else:
-            max_feature_threshold = self.max_feature_threshold
 
         # loop through cv folds
         if self.train_test_splits is None:
@@ -182,12 +171,8 @@ class KFSIFR(BaseEstimator):
                         f_ratios = np.divide(a, b, out=np.zeros_like(a), where=b!=0)
                         try:
                             id = np.where(f_ratios > self.jump_ratio)[0][0]
-                            if (id + 1) < max_feature_threshold:
-                                S = S[:id]
-                                S = Sc[S]
-                            else:
-                                print("Maximum feature threshold exceeded, no features selected...")
-                                break
+                            S = S[:id]
+                            S = Sc[S]
                         except IndexError:
                             print("Jump failed, no features selected...")
                             break
@@ -204,7 +189,7 @@ class KFSIFR(BaseEstimator):
                     print("Feature set size = " + str(Sij.size))
 
                     # check threshold condition
-                    if score < self.gamma:
+                    if bsr < self.gamma:
                         break
 
                 # set selected features
