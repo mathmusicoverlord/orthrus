e09e08d94bad29dbf9dc7c774e78278a6edc6124
ekeho <ekehoe32@gmail.com>
2021-06-15 17:22:55 -0600

    - added median-fold-change normalization

--- datasci/preprocessing/normalizations.py
+++ datasci/preprocessing/normalizations.py
@@ -1,4 +1,5 @@
 import numpy as np
+from copy import deepcopy
 '''
 This module defines various classes to normalize the dataset. Class instance 
 must contain the method fit_transform. The output of normalizer.fit_transform(:py:attr:`DataSet.data`) 
@@ -12,3 +13,38 @@ class LogNormalizer():
             data[np.isneginf(data)] = 0
 
         return data
+
+class MedianFoldChangeNormalizer():
+    def fit_transform(self, X, controls=None):
+
+        # recast as numpy array is necessary
+        Z = deepcopy(np.array(X))
+
+        # set default for controls
+        if controls is None:
+            controls = np.arange(Z.shape[0])
+
+        # Find missing data and change zero to nan
+        missing = (Z == 0)
+        Z[missing] = np.nan
+
+        # Integral normalize
+        integral = np.nansum(Z, axis=1)
+        integral = integral.reshape(-1, 1)
+        Z = 100 * (Z / integral)
+
+        # Calculate medians of controls for "Golden Reference"
+        median = np.nanmedian(Z[controls, :], axis=0)
+
+        # Calculate fold changes from median
+        quotients = Z / median
+
+        # Calculate median of fold changes
+        median_quotients = np.nanmedian(quotients, axis=1)
+        median_quotients = median_quotients.reshape(-1, 1)
+
+        # Scale each sample by median of the fold changes
+        Z = Z / median_quotients
+        Z[missing] = 0
+
+        return Z
