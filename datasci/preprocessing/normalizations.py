846102c0d849be9dfda2c18e2daec594901f4578
Kartikay Sharma <sharmas.kartik@gmail.com>
2021-07-08 17:52:30 -0600

    Updated docs

--- datasci/preprocessing/normalizations.py
+++ datasci/preprocessing/normalizations.py
@@ -6,9 +6,17 @@ must have the same number of columns as :py:attr:`DataSet.data`
 '''
 
 class LogNormalizer():
-    def fit_transform(self, data):
+    def fit_transform(self, X):
+        """
+        Applies element wise log to the data. Any values that are negative infinity are set to zero after applying log.
+
+        Args:
+            X (ndarray of shape (m, n))): array of data, with m the number of observations in R^n.
+        Return:
+            (ndarray of shape (m, n))): Modified data matrix.
+        """
         with np.errstate(divide='ignore'):
-            data = np.log(data)
+            data = np.log(X)
             data[np.isneginf(data)] = 0
 
         return data
