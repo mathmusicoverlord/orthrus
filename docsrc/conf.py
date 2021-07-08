9a2a211245ddb899966f3bb2f314c89023d318a1
Eric Kehoe <ekehoe32@gmail.com>
2021-07-08 16:31:18 -0600

    updated docs

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -40,6 +40,8 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.todo',
     'sphinx.ext.mathjax']
 
+napoleon_use_ivar = True
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
