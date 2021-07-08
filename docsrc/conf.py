c3b626ac9c31dea8b8548b7d2d039bf449418e0c
Eric Kehoe <ekehoe32@gmail.com>
2021-07-08 16:19:14 -0600

    updated docs

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -40,8 +40,6 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.todo',
     'sphinx.ext.mathjax']
 
-autodoc_default_options = {'members': True, 'member-order': 'bysource', 'special-members': '__init__'}
-
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
