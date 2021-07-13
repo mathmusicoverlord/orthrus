6ba444a29ab29b9668c337937e9cdf95b12d9398
Eric Kehoe <ekehoe32@gmail.com>
2021-07-13 16:11:30 -0600

    - added progress bar to pathway_selection code

--- datasci/sparse/feature_selection/pathway_selection.py
+++ datasci/sparse/feature_selection/pathway_selection.py
@@ -8,16 +8,17 @@ from sklearn.utils.multiclass import unique_labels
 import ray
 import numpy as np
 import torch as tc
+from tqdm import tqdm
 
 class PathwayScore(BaseEstimator):
 
     def __init__(self,
                  device: int = -1,
-                 use_ray: bool = True):
+                 parallel: bool = True):
 
         # parameters
         self.device = device
-        self.use_ray = use_ray
+        self.parallel = parallel
 
         # attributes
         self.X_ = None
@@ -40,46 +41,69 @@ class PathwayScore(BaseEstimator):
         self.pathways_ = pathways
 
         # generate subspaces
+        if self.parallel:
+            ray.init(local_mode=False)
+        else:
+            ray.init(local_mode=True)
+            self.subspaces_ = []
+
+        # define generate_subspace function
+        @ray.remote
+        def generate_subspace(X, sample_ids, feature_ids):
+
+            # restrict the data
+            Z = X[:, feature_ids]
+            Z = Z[sample_ids, :]
+            Z = self.convert_type(Z)
+
+            # perform SVD to extract subspace
+            _, _, V = tc.svd(Z)
+
+            # check if there are more samples than features
+            if len(sample_ids) >= len(feature_ids):
+                Z = V[:, :-1].transpose(0, 1)  # use best n-1 singular vectors for basis
+            else:
+                Z = V.transpose(0, 1)
+
+            assert Z is not None, 'Something is wrong here...'
+
+            return Z.detach().cpu().numpy()
+
+        # find pathway subspaces
+        futures = []
+        for cls in self.classes_:
+            for pathway in pathways:
+                sample_ids = np.where(self.y_ == cls)[0]
+                futures.append(generate_subspace.remote(self.X_, sample_ids, pathway))
+
+        # setup progess bar
         print("Generating subspaces...")
-        if self.use_ray:
-            # initialize ray cluster
-            ray.init()
+        bar = tqdm(total=len(self.classes_)*len(pathways))
+        was_ready = []
+        total = 0
+        # get futures
+        while True:
+            ready, not_ready = ray.wait(futures)
+            delta_ready = list(set(ready).difference(set(was_ready)))
+            if not self.parallel:
+                self.subspaces_.append(ray.get(delta_ready))
+            if len(delta_ready) > 0:
+                bar.update(len(delta_ready))
+                total = total + len(delta_ready)
+            if total == bar.total:
+                break
+            was_ready = ready
+        bar.close()
 
-            # define generate_subspace function
-            @ray.remote
-            def generate_subspace(X, sample_ids, feature_ids):
-
-                # restrict the data
-                Z = X[:, feature_ids]
-                Z = Z[sample_ids, :]
-                Z = self.convert_type(Z)
-
-                # perform SVD to extract subspace
-                _, _, V = tc.svd(Z)
-
-                # check if there are more samples than features
-                if len(sample_ids) >= len(feature_ids):
-                    Z = V[:, :-1].transpose(0, 1)  # use best n-1 singular vectors for basis
-                else:
-                    Z = V.transpose(0, 1)
-
-                    return Z.detach().cpu().numpy()
-
-            # find pathway subspaces
-            futures = []
-            for cls in self.classes_:
-                for pathway in pathways:
-                    sample_ids = np.where(self.y_ == cls)[0]
-                    futures.append(generate_subspace.remote(self.X_, sample_ids, pathway))
-
-            # get futures
+        if self.parallel:
+            print("Collecting subspaces from workers...")
             self.subspaces_ = ray.get(futures)
 
-            # reshape the list of subspaces
-            self.subspaces_ = np.array(self.subspaces_).reshape(len(self.classes_), -1)
-        else:
-            pass
+        # reshape the list of subspaces
+        self.subspaces_ = np.array(self.subspaces_, dtype=object).reshape(len(self.classes_), -1)
 
+        # shutdown ray
+        ray.shutdown()
 
     def transform(self, X, y=None):
 
@@ -90,36 +114,63 @@ class PathwayScore(BaseEstimator):
         X = np.linalg.norm(X, axis=1, keepdims=True)
 
         # compute angles
-        if self.use_ray:
-
-            # define angle function
-            @ray.remote
-            def angle(X, Y):
-
-                # convert types
-                Z = self.convert_type(X)
-                W = self.convert_type(Y)
-
-                # compute product
-                angles = tc.matmul(W, Z.transpose(0, 1))
-                angles = tc.linalg.norm(angles, axis=1, keepdim=True)
-                angles = tc.arccos(angles)
-
-                return angles.detach().cpu().numpy().tolist()
-
-            # find angles between incoming samples and pathway subspaces
-            futures = []
-            for i, cls in enumerate(self.classes_):
-                for j, pathway in enumerate(self.pathways_):
-                    sample_ids = np.where(self.y_ == cls)[0]
-                    futures.append(angle.remote(self.subspaces_[i, j], X))
-
-            # get futures
-            self.scores_ = ray.get(futures)
-            self.scores_ = np.array(self.scores_).reshape(len(self.classes_), len(self.pathways_), X.shape[0])
-
+        if self.parallel:
+            ray.init(local_mode=False)
         else:
-            pass
+            ray.init(local_mode=True)
+            self.scores_ = []
+
+        # define angle function
+        @ray.remote
+        def angle(X, Y):
+
+            # convert types
+            Z = self.convert_type(X)
+            W = self.convert_type(Y)
+
+            # compute product
+            angles = tc.matmul(W, Z.transpose(0, 1))
+            angles = tc.linalg.norm(angles, axis=1, keepdim=True)
+            angles = tc.arccos(angles)
+
+            return angles.detach().cpu().numpy().tolist()
+
+        # find angles between incoming samples and pathway subspaces
+        futures = []
+        for i, cls in enumerate(self.classes_):
+            for j, pathway in enumerate(self.pathways_):
+                futures.append(angle.remote(self.subspaces_[i, j], X))
+
+        # setup progess bar
+        print("Computing angles...")
+        bar = tqdm(total=len(self.classes_)*len(self.pathways_))
+        was_ready = []
+        total = 0
+        # get futures
+        while True:
+            ready, not_ready = ray.wait(futures)
+            delta_ready = list(set(ready).difference(set(was_ready)))
+            if not self.parallel:
+                self.scores_.append(ray.get(delta_ready))
+            if len(delta_ready) > 0:
+                bar.update(len(delta_ready))
+                total = total + len(delta_ready)
+            if total == bar.total:
+                break
+            was_ready = ready
+        bar.close()
+
+        # get futures
+        if self.parallel:
+            print("Collecting angle results from workers...")
+            self.scores_ = ray.get(futures)
+        self.scores_ = np.array(self.scores_).reshape(len(self.classes_), len(self.pathways_), X.shape[0])
+
+        # shutdown ray
+        ray.shutdown()
+
+        # return
+        return self.scores_
 
     def convert_type(self, x):
 
