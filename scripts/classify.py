4aaf738415601b0f1d28c7843ea25868afc627fe
Eric Kehoe <ekehoe32@gmail.com>
2021-07-27 18:47:38 -0600

    -Updated classify.py to work with new product_parallelize.py script

--- scripts/classify.py
+++ scripts/classify.py
@@ -2,88 +2,141 @@
 Generic script for classifying a dataset.
 """
 
-if __name__ == '__main__':
-    # imports for arguments
-    import argparse
-    import os
 
-    # command line arguments
-    parser = argparse.ArgumentParser("generic-classification")
+# imports for arguments
+import argparse
+import os
 
-    parser.add_argument('--exp_params',
-                        type=str,
-                        default=os.path.join(os.environ['DATASCI_PATH'], 'test_data', 'Iris', 'Experiments',
-                                             'setosa_versicolor_classify_species_svm',
-                                             'setosa_versicolor_classify_species_svm_params.py'),
-                        help='File path of containing the experimental parameters. Default is the Iris experiment.')
+# command line arguments
+parser = argparse.ArgumentParser("generic-classification")
 
-    parser.add_argument('--score',
-                        type=str,
-                        default='bsr',
-                        choices=['bsr', 'accuracy', 'confusion'],
-                        help='Score reported on the prediction results.')
+parser.add_argument('--exp_params',
+                    type=str,
+                    default=os.path.join(os.environ['DATASCI_PATH'], 'test_data', 'Iris', 'Experiments',
+                                         'setosa_versicolor_classify_species_svm',
+                                         'setosa_versicolor_classify_species_svm_params.py'),
+                    help='File path of containing the experimental parameters. Default is the Iris experiment.')
 
-    args = parser.parse_args()
+parser.add_argument('--score',
+                    type=str,
+                    default='bsr',
+                    choices=['bsr', 'accuracy', 'confusion'],
+                    help='Score reported on the prediction results.')
 
-    # imports
-    from datasci.core.helper import save_object
-    from datasci.core.helper import module_from_path
-    from datasci.core.helper import default_val
+args = parser.parse_args()
 
-    # set experiment parameters
-    exp_params = module_from_path('exp_params', args.exp_params)
+# imports
+from datasci.core.helper import save_object, module_from_path, default_val, pop_first_element as pop
+
+# set experiment parameters
+exp_params = module_from_path('exp_params', args.exp_params)
+try:
     script_args = exp_params.CLASSIFY_ARGS
+except AttributeError:
+    script_args = {}
 
-    ## required script params
-    results_dir = script_args.get('RESULTS_DIR', exp_params.RESULTS_DIR)
-    exp_name = script_args.get('EXP_NAME', exp_params.EXP_NAME)
-    class_attr = script_args.get('CLASS_ATTR', exp_params.CLASS_ATTR)
-    ds = script_args.get('DATASET', exp_params.DATASET)
-    classifier = script_args.get('CLASSIFIER', exp_params.CLASSIFIER)
+## required script params
+results_dir = script_args.get('RESULTS_DIR', exp_params.RESULTS_DIR)
+exp_name = script_args.get('EXP_NAME', exp_params.EXP_NAME)
+attr = script_args.get('CLASS_ATTR', exp_params.CLASS_ATTR)
+ds = script_args.get('DATASET', exp_params.DATASET)
+classifier = script_args.get('CLASSIFIER', default_val(exp_params, 'CLASSIFIER'))
 
-    ## optional script params
-    sample_ids = script_args.get('SAMPLE_IDS',  default_val(exp_params, 'SAMPLE_IDS')),
-    feature_ids = script_args.get('FEATURE_IDS', default_val(exp_params, 'FEATURE_IDS'))
-    classifier_name = script_args.get('CLASSIFIER_NAME', default_val(exp_params, 'CLASSIFIER_NAME'))
-    scorer_args = script_args.get('SCORER_ARGS', default_val(exp_params, 'CLASSIFY_SCORER_ARGS'))
-    classifier_fweights_handle = script_args.get('CLASSIFIER_FWEIGHTS_HANDLE',
-                                                 default_val(exp_params, 'CLASSIFIER_FWEIGHTS_HANDLE'))
-    classifier_sweights_handle = script_args.get('CLASSIFIER_SWEIGHTS_HANDLE',
-                                                 default_val(exp_params, 'CLASSIFIER_SWEIGHTS_HANDLE'))
-    partitioner = script_args.get('PARTITIONER', default_val(exp_params, 'PARTITIONER'))
-    partitioner_name = script_args.get('PARTITIONER_NAME', default_val(exp_params, 'PARTITIONER_NAME'))
-    results_file_name = script_args.get('RESULTS_FILE_NAME',
-                                        default_val(exp_params, 'CLASSIFICATION_RESULTS_FILE_NAME'))
+## optional script params
+sample_ids = script_args.get('SAMPLE_IDS',  default_val(exp_params, 'SAMPLE_IDS')),
+feature_ids = script_args.get('FEATURE_IDS', default_val(exp_params, 'FEATURE_IDS'))
+classifier_name = script_args.get('CLASSIFIER_NAME', default_val(exp_params, 'CLASSIFIER_NAME'))
+scorer_args = script_args.get('SCORER_ARGS', default_val(exp_params, 'CLASSIFY_SCORER_ARGS', {}))
+f_weights_handle = script_args.get('CLASSIFIER_F_WEIGHTS_HANDLE', default_val(exp_params, 'CLASSIFIER_F_WEIGHTS_HANDLE'))
+s_weights_handle = script_args.get('CLASSIFIER_S_WEIGHTS_HANDLE', default_val(exp_params, 'CLASSIFIER_S_WEIGHTS_HANDLE'))
+partitioner = script_args.get('PARTITIONER', default_val(exp_params, 'PARTITIONER'))
+partitioner_name = script_args.get('PARTITIONER_NAME', default_val(exp_params, 'PARTITIONER_NAME'))
+results_file_name = script_args.get('RESULTS_FILE_NAME',
+                                    default_val(exp_params, 'CLASSIFICATION_RESULTS_FILE_NAME'))
 
-    # set scorer
-    if args.score == 'bsr':
-        from sklearn.metrics import balanced_accuracy_score
-        scorer = balanced_accuracy_score
-    elif args.score == 'confusion':
-        from sklearn.metrics import confusion_matrix
-        scorer = confusion_matrix
-    elif args.score == 'accuracy':
-        from sklearn.metrics import accuracy_score
-        scorer = accuracy_score
+# set scorer
+scorer_name = args.score
+if args.score == 'bsr':
+    from sklearn.metrics import balanced_accuracy_score
+    scorer = balanced_accuracy_score
+elif args.score == 'confusion':
+    from sklearn.metrics import confusion_matrix
+    scorer = confusion_matrix
+elif args.score == 'accuracy':
+    from sklearn.metrics import accuracy_score
+    scorer = accuracy_score
+
+# define the script run function
+def run(ds,
+        classifier,
+        classifier_name,
+        attr,
+        sample_ids,
+        feature_ids,
+        partitioner,
+        partitioner_name,
+        scorer,
+        scorer_name,
+        scorer_args,
+        f_weights_handle,
+        s_weights_handle):
 
     # classify data
     classification_results = ds.classify(classifier=classifier,
                                          classifier_name=classifier_name,
-                                         attr=class_attr,
+                                         attr=attr,
                                          sample_ids=sample_ids,
                                          feature_ids=feature_ids,
                                          partitioner=partitioner,
                                          partitioner_name=partitioner_name,
                                          scorer=scorer,
-                                         scorer_name=args.score,
+                                         scorer_name=scorer_name,
                                          scorer_args=scorer_args,
-                                         f_weights_handle=classifier_fweights_handle,
-                                         s_weights_handle=classifier_sweights_handle)
+                                         f_weights_handle=f_weights_handle,
+                                         s_weights_handle=s_weights_handle)
+
+    return classification_results
+
+# define the script save function
+def save(result, num=None):
+    # check for non-parallel job
+    if num is None:
+        num = ''
 
     # save classification results
     if results_file_name is None:
-        save_object(classification_results,
+        save_object(result,
                     os.path.join(results_dir,
-                                 '_'.join([ds.name, exp_name, args.score, 'classification_results.pickle'])))
+                                 '_'.join([ds.name,
+                                           exp_name,
+                                           args.score,
+                                           'classification',
+                                           'results',
+                                           str(num) + '.pickle']
+                                          )
+                                 )
+                    )
     else:
-        save_object(classification_results, os.path.join(results_dir, results_file_name))
+        save_object(result,
+                    os.path.join(results_dir, '_'.join([results_file_name, str(num) + '.pickle']))
+                    )
+
+
+if __name__ == '__main__':
+
+    # run the script
+    classification_results = run(ds=pop(ds),
+                                 classifier=pop(classifier),
+                                 classifier_name=pop(classifier_name),
+                                 attr=pop(attr),
+                                 sample_ids=pop(sample_ids),
+                                 feature_ids=pop(feature_ids),
+                                 partitioner=pop(partitioner),
+                                 partitioner_name=pop(partitioner_name),
+                                 scorer=pop(scorer),
+                                 scorer_name=pop(scorer_name),
+                                 scorer_args=pop(scorer_args),
+                                 f_weights_handle=pop(f_weights_handle),
+                                 s_weights_handle=pop(s_weights_handle))
+    # save the results
+    save(classification_results)
