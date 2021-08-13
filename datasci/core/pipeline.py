0b30535f736f2a866c7389482d6b7e90d1ba54b6
Eric Kehoe <ekehoe32@gmail.com>
2021-08-12 19:15:45 -0600

    - Prototyping Process class

--- datasci/core/pipeline.py
+++ datasci/core/pipeline.py
@@ -13,17 +13,19 @@ from datasci.core.helper import save_object
 class Process(ABC):
 
     def __init__(self,
-                 process: object,
-                 process_name: str = None,
+                 process:object,
+                 process_name:str = None,
+                 parallel:bool = True
                  ):
 
         # set parameters
         self.process = process
         self._process_name = process_name
+        self.parallel = parallel
 
         # set attributes
         self.run_status_ = -1
-        self.result_ = None
+        self.results_ = None
 
     @property
     def process_name(self):
@@ -34,15 +36,67 @@ class Process(ABC):
 
         return self._process_name
 
-    @abstractmethod
-    def run(self, ds: DataSet, **kwargs):
-        pass
+    def run_(self, ds:DataSet, **kwargs):
+
+        ## prep for subclasses run_ method ##
+
+        # grab transformation object
+        transform = kwargs.get('transform', lambda x: x)
+        ds_new = transform(ds)
+
+        return ds_new
+
+    def run(self, ds:DataSet, batch_args:dict = None):
+
+        if batch_args is None:
+            self.results_ = dict(batch=self.run_(ds))
+            return
+
+        # setup for sequential or parallel processesing
+        if self.parallel:
+            self.results_ = self.run_par_(ds, batch_args)
+        else:
+            # intialize results
+            self.results_ = dict()
+            for batch in batch_args:
+                self.results_[batch] = self.run_(ds, **batch_args[batch])
+
+    def run_par_(self, ds:DataSet, batch_args: dict):
+
+        # define a remote run_ method
+        run_ = ray.remote(self.run_)
+
+        # store dataset in the object store
+        if ds.__class__ == ray._raylet.ObjectRef:
+            ds_remote = ds
+        else:
+            ds_remote = ray.put(ds)
+
+        # naive ray parallelization
+        futures = []
+        for batch in batch_args:
+            futures.append(run_.remote(ds_remote, **batch_args[batch]))
+
+        # collect results
+        return dict(zip(batch_args.keys(), ray.get(futures)))
 
     @abstractmethod
-    def save(self,
+    def save_results(self,
              save_path: str,
              overwrite: bool = False):
         pass
+
+    def save(self,
+             save_path: str,
+             overwrite: bool = False):
+
+        # generate new save path in case you don't want to overwrite
+        save_path = generate_save_path(save_path, overwrite)
+
+        # save instance of object
+        save_object(self, save_path)
+
+
 
 
 class Partition(Process):
@@ -69,7 +123,10 @@ class Partition(Process):
 
         # set attributes
 
-    def run(self, ds: DataSet, **kwargs):
+    def run_(self, ds:DataSet, **kwargs):
+
+        # run the supers run_ method
+        ds_new = super(Partition, self).run_(ds, **kwargs)
 
         # generate the split method
         split = eval("self.process." + self.split_handle)
@@ -78,20 +135,20 @@ class Partition(Process):
         if self.split_attr is None:
             label_dict = {}
         else:
-            label_dict = dict(y=ds.metadata[self.split_attr])
+            label_dict = dict(y=ds_new.metadata[self.split_attr])
 
         if self.split_group is not None:
             label_dict['groups'] = self.split_group
 
         # partition the dataset
-        parts = split(ds.data, **label_dict, **self.split_args)
+        parts = split(ds_new.data, **label_dict, **self.split_args)
 
         # record training and test labels
-        train_test_labels = pd.DataFrame(index=ds.metadata.index)
+        train_test_labels = pd.DataFrame(index=ds_new.metadata.index)
         for i, (train_idx, test_idx) in enumerate(parts):
 
             # create new column for split
-            col_name = 'split_' + str(i)
+            col_name = 'batch_' + str(i)
             train_test_labels[col_name] = ''
 
             # fill in training and test
@@ -99,19 +156,17 @@ class Partition(Process):
             train_test_labels.iloc[test_idx, i] = 'Test'
 
         # store the result
-        self.result_ = train_test_labels
+        return train_test_labels
 
-        return ds
-
-    def save(self,
-             save_path: str,
-             overwrite: bool = False):
+    def save_result(self,
+                    save_path: str,
+                    overwrite: bool = False):
 
         # generate new save path in case you don't want to overwrite
         save_path = generate_save_path(save_path, overwrite)
 
         # save labels as .csv
-        self.result_.to_csv(save_path)
+        self.results_.to_csv(save_path)
 
 
 
