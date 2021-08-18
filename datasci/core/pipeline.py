fe511188485ccadad8bedb6b8a63cba14de9132c
Eric Kehoe <ekehoe32@gmail.com>
2021-08-18 17:42:29 -0600

    - Added Fit parent process to Transform and Classify - Added Classify process prototype

--- datasci/core/pipeline.py
+++ datasci/core/pipeline.py
@@ -16,21 +16,23 @@ import ray
 class Process(ABC):
 
     def __init__(self,
-                 process:object,
-                 process_name:str = None,
-                 parallel:bool = False,
+                 process: object,
+                 process_name: str = None,
+                 parallel: bool = False,
                  verbosity: int = 0,
                  ):
 
         # set parameters
         self.process = process
-        self._process_name = process_name
         self.parallel = parallel
         self.verbosity = verbosity
 
         # set attributes
-        self.run_status_ = -1
+        self.run_status_ = 0
         self.results_ = None
+
+        # private
+        self._process_name = process_name
 
     @property
     def process_name(self):
@@ -41,9 +43,9 @@ class Process(ABC):
 
         return self._process_name
 
-    def preprocess_(self, ds:DataSet, **kwargs):
+    def _preprocess(self, ds:DataSet, **kwargs):
 
-        ## prep for subclasses run_ method ##
+        ## prep for subclasses _run method ##
 
         # grab transformation object
         transform = kwargs.get('transform', lambda x: x)
@@ -52,34 +54,37 @@ class Process(ABC):
         return ds_new
 
     @abstractmethod
-    def run_(self, ds:DataSet, **kwargs):
+    def _run(self, ds:DataSet, **kwargs):
         pass
 
     def run(self, ds:DataSet, batch_args:dict = None):
 
         if batch_args is None:
-            self.results_ = dict(batch=self.run_(ds))
+            self.results_ = dict(batch=self._run(ds))
             return
 
         # setup for sequential or parallel processesing
         if self.parallel:
-            self.results_ = self.run_par_(ds, batch_args)
+            self.results_ = self._run_par(ds, batch_args)
         else:
             # intialize results
             self.results_ = dict()
             for batch in batch_args:
                 if self.verbosity > 0:
                     print(batch + ':')
-                self.results_[batch] = self.run_(ds, **batch_args[batch])
+                self.results_[batch] = self._run(ds, **batch_args[batch])
                 if self.verbosity > 0:
                     print()
 
+        # change run status
+        self.run_status_ = 1
+
         return ds, self.results_
 
-    def run_par_(self, ds:DataSet, batch_args: dict):
+    def _run_par(self, ds: DataSet, batch_args: dict):
 
-        # define a remote run_ method
-        run_ = ray.remote(lambda *args, **kwargs: self.run_(*args, **kwargs))
+        # define a remote _run method
+        _run = ray.remote(lambda *args, **kwargs: self._run(*args, **kwargs))
 
         # store dataset in the object store
         if ds.__class__ == ray._raylet.ObjectRef:
@@ -90,7 +95,7 @@ class Process(ABC):
         # naive ray parallelization
         futures = []
         for batch in batch_args:
-            futures.append(run_.remote(ds_remote, **batch_args[batch]))
+            futures.append(_run.remote(ds_remote, **batch_args[batch]))
 
         # collect results
         return dict(zip(batch_args.keys(), ray.get(futures)))
@@ -138,10 +143,10 @@ class Partition(Process):
         self.split_args = split_args
         self.split_handle = split_handle
 
-    def run_(self, ds:DataSet, **kwargs):
+    def _run(self, ds: DataSet, **kwargs):
 
-        # run the supers preprocess_ method
-        ds_new = self.preprocess_(ds, **kwargs)
+        # run the supers _preprocess method
+        ds_new = self._preprocess(ds, **kwargs)
 
         # check for validation in existing labels
         tvt_labels = kwargs.get('tvt_labels', None)
@@ -233,6 +238,9 @@ class Partition(Process):
         # update results
         self.results_ = results
 
+        # change run status
+        self.run_status_ = 1
+
         return ds, self.results_
 
     def save_results(self,
@@ -249,19 +257,74 @@ class Partition(Process):
         results.to_csv(save_path)
 
 
-class Transform(Process):
+class Fit(Process):
 
     def __init__(self,
                  process: object,
                  process_name: str = None,
                  parallel: bool = False,
                  verbosity: int = 0,
-                 by_coordinate: bool = False,
+                 supervised_attr: str = None,
+                 fit_handle: str = 'fit',
+                 fit_args: dict = {},
+                 ):
+
+        super(Fit, self).__init__(process=process,
+                                  process_name=process_name,
+                                  parallel=parallel,
+                                  verbosity=verbosity,
+                                  )
+
+        # set parameters
+        self.supervised_attr = supervised_attr
+        self.fit_args = fit_args
+
+        # set private attributes
+        self._fit_handle = fit_handle
+
+    def _extract_training_ids(self, ds: DataSet, **kwargs):
+
+        # grab training labels
+        tvt_labels = kwargs.get('tvt_labels', None)
+        if tvt_labels is None:
+            training_ids = ds.metadata.index
+        else:
+            training_ids = (tvt_labels == 'Train')
+
+        return training_ids
+
+    def _fit(self, ds: DataSet, **kwargs):
+
+        # extract training ids
+        training_ids = self._extract_training_ids(ds, **kwargs)
+
+        #  add supervised labels to fit args
+        if self.supervised_attr is not None:
+            y = ds.metadata.loc[training_ids, self.supervised_attr]
+            self.fit_args['y'] = y
+
+        # fit the process
+        if self.verbosity > 0:
+            print(r"Fitting %s..." % (self.process_name,))
+        process = deepcopy(self.process)
+        process = eval("process." + self._fit_handle)(ds.data.loc[training_ids], **self.fit_args)
+
+        return process
+
+
+class Transform(Fit):
+
+    def __init__(self,
+                 process: object,
+                 process_name: str = None,
+                 parallel: bool = False,
+                 verbosity: int = 0,
+                 retain_f_ids: bool = False,
                  fit_handle: str = 'fit',
                  transform_handle: str = 'transform',
                  fit_transform_handle: str = 'fit_transform',
                  supervised_attr: str = None,
-                 transform_args: dict = {}):
+                 fit_args: dict = {}):
 
 
         # init with Process class
@@ -269,96 +332,186 @@ class Transform(Process):
                                         process_name=process_name,
                                         parallel=parallel,
                                         verbosity=verbosity,
+                                        supervised_attr=supervised_attr,
+                                        fit_handle=fit_handle,
+                                        fit_args=fit_args,
                                         )
 
-        # set private parameters
-        self._fit_handle = fit_handle
+        # set parameters
+        self.retain_f_ids = retain_f_ids
+
+        # set private attributes
         self._transform_handle = transform_handle
         self._fit_transform_handle = fit_transform_handle
-
-        # set public parameters
-        self.by_coordinate = by_coordinate
-        self.supervised_attr = supervised_attr
-        self.transform_args = transform_args
 
         # check appropriate parameters
         if self._fit_handle is None or self._transform_handle is None:
             if self._fit_transform_handle is None:
-                raise ValueError("Transform must have either both a fit method and a transform method or just a fit_transform method!")
+                raise ValueError("Transform process must have either both a fit method and a transform method or just a fit_transform method!")
             else:
                 warnings.warn("Transform will use its fit_transform method to fit."
                               " Make sure that its fit_transform method fits"
                               " the transformation inplace!")
 
-    def run_(self, ds:DataSet, **kwargs):
+    def _fit_transform(self, ds: DataSet, **kwargs):
 
-        # run the super's preprocess_ method
-        ds_new = self.preprocess_(ds, **kwargs)
+        # extract training ids
+        training_ids = self._extract_training_ids(ds, **kwargs)
+
+        #  add supervised labels to fit args
+        if self.supervised_attr is not None:
+            y = ds.metadata.loc[training_ids, self.supervised_attr]
+            self.fit_args['y'] = y
+
+        # fit the process
+        if self.verbosity > 0:
+            print(r"Fitting %s and then transforming data..." % (self.process_name,))
+        process = deepcopy(self.process)
+        data = eval("process." + self._fit_transform_handle)(ds.data.loc[training_ids], **self.fit_args)
+
+        return process, data
+
+    def _run(self, ds:DataSet, **kwargs):
 
         # initalize output
         result = dict()
 
-        # grab training labels
-        tvt_labels = kwargs.get('tvt_labels', None)
-        if tvt_labels is None or self._transform_handle is None:
-            training_ids = ds_new.index
-        else:
-            training_ids = (tvt_labels == 'Train')
+        # run the super's _preprocess method
+        ds_new = self._preprocess(ds, **kwargs)
 
-        #  fit the transform
-        if self.supervised_attr is not None:
-            y = ds.metadata.loc[training_ids, self.supervised_attr]
-            self.transform_args['y'] = y
-
+        # attempt to fit
         if self._transform_handle is not None:
-            if self.verbosity > 0:
-                print(r"Fitting %s..." % (self.process_name,))
-            process = deepcopy(self.process)
             if self._fit_handle is not None:
-                process = eval("process." + self._fit_handle)(ds.data.loc[training_ids], **self.transform_args)
+                process = self._fit(ds_new, **kwargs)
             else:
-                eval("process." + self._fit_transform_handle)(ds.data.loc[training_ids], **self.transform_args)
+                process, _ = self._fit_transform(ds_new, **kwargs)
         else:
             process = None
 
-        # define transform object for a dataset
-        def transform(ds:DataSet):
+        # store resulting transform
+        result['transform'] = self._generate_transform(process)
+
+        return result
+
+    def _generate_transform(self, process=None):
+
+        # define transform
+        def transform(ds: DataSet):
             if process is None:
-                if self.verbosity > 0:
-                    print(r"Fitting %s..." % (self.process_name,))
-                data_new = eval("process." + self._fit_transform_handle)(ds.data, **self.transform_args)
+                _, data_new = self._fit_transform(ds)
             else:
+                if self.verbosity > 0:
+                    print(r"Transforming the data using %s..." % (self.process_name,))
                 data_new = eval("process." + self._transform_handle)(ds.data)
-            if self.by_coordinate:
+            if self.retain_f_ids:
                 try:
                     data_new = pd.DataFrame(data=data_new, index=ds.data.index, columns=ds.data.columns)
                     ds_new = deepcopy(ds)
                     ds_new.data = data_new
                 except ValueError:
-                    raise ValueError("Transform changes the dimension of the data and therefore cannot be a coordinate-by-coordinate transformation!")
+                    raise ValueError("Transform changes the dimension of the data and therefore cannot retain the original feature ids in the new dataset!")
             else:
                 data_new = pd.DataFrame(data=data_new, index=ds.data.index, columns=['_'.join([self.process_name, str(i)]) for i in range(data_new.shape[1])])
+
+                # check if features are the same after transformation and use original feature ids for columns
                 ds_new = DataSet(data=data_new, metadata=ds.metadata)
 
             return ds_new
 
-        # store resulting transform
-        result['transform'] = transform
+        return transform
 
-        return result
+    def transform(self, ds: DataSet):
+
+        # transform the incoming data according to transforms
+        try:
+            out = {k: v['transform'](ds) for (k, v) in self.results_.items()}
+            return out
+        except TypeError:
+            raise RuntimeError("Transform must call its run method on a dataset before it can transform a new dataset!")
+
+    def save_results(self, save_path: str, overwrite: bool = False):
+
+        # call save_object on results
+        save_object(self.results_, save_path, overwrite)
+
+
+class Classify(Fit):
+
+    def __init__(self,
+                 process: object,
+                 class_attr: str,
+                 process_name: str = None,
+                 parallel: bool = False,
+                 verbosity: int = 0,
+                 fit_handle: str = 'fit',
+                 predict_handle: str = 'predict',
+                 fit_args: dict = {},
+                 predict_args: dict = {},
+                 classes_handle='classes_',
+                 ):
+
+        # init with Process class
+        super(Classify, self).__init__(process=process,
+                                       process_name=process_name,
+                                       parallel=parallel,
+                                       verbosity=verbosity,
+                                       supervised_attr=class_attr,
+                                       fit_handle=fit_handle,
+                                       fit_args=fit_args,
+                                       )
+
+        # set parameters
+        self.fit_args = fit_args
+        self.predict_args = predict_args
+        self.class_attr = self.supervised_attr # shadows supervised_attr
+
+        # set private attributes
+        self._predict_handle = predict_handle
+        self._classes_handle = classes_handle
+
+        # check appropriate parameters
+        if self._fit_handle is None or self._predict_handle is None:
+            raise ValueError("Classify process must have both a fit method and a predict method!")
 
 
     def save_results(self, save_path: str, overwrite: bool = False):
         pass
 
+    def _run(self, ds: DataSet, **kwargs):
 
+        # initalize output
+        result = dict()
 
+        # run the super's _preprocess method
+        ds_new = self._preprocess(ds, **kwargs)
 
+        # fit the classifier
+        process = self._fit(ds_new, **kwargs)
 
+        # store resulting transform
+        pred, pred_label = self._generate_labels_or_scores(process)
+        result[pred_label] = pred
 
+        return result
 
+    def _generate_labels_or_scores(self, process: object, ds: DataSet):
 
+        # predict on dataset
+        if self.verbosity > 0:
+            print(r"Classifying the data using %s..." % (self.process_name,))
+        predictions = eval("process." + self._predict_handle)(ds.data)
 
+        # format output as series if labels
+        try:
+            predictions.shape[1]
+            pred = pd.DataFrame(data=predictions, index=ds.metadata.index, columns=eval("process." + self._classes_handle))
+            pred.name = self.process_name + " scores"
+            pred_label = 'class_scores'
+        except IndexError:
+            pred = pd.Series(name=self.process_name + " labels", data=predictions, index=ds.metadata.index)
+            pred_label = 'class_labels'
+
+        return pred, pred_label
 
 
 
