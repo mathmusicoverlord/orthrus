6dea00ad2665787e07934096081c6d8cf353b55e
Eric Kehoe <ekehoe32@gmail.com>
2021-08-19 15:18:45 -0600

    - Added module function for compressing dict of dict of pandas - Added Process level compressing method for results - Added Process level save_results method

--- datasci/core/pipeline.py
+++ datasci/core/pipeline.py
@@ -3,8 +3,12 @@ This module contains the classes and functions associated with pipeline componen
 """
 
 from abc import ABC, abstractmethod
+from typing import Union
 import os
 import pandas as pd
+from numpy import ndarray
+import numpy as np
+from pandas import DataFrame, Series
 from datasci.core.dataset import DataSet
 from datasci.core.helper import generate_save_path
 from datasci.core.helper import save_object
@@ -12,6 +16,33 @@ import warnings
 from copy import deepcopy
 import ray
 
+# module functions
+def _compress_dict_dict_pandas(kv: dict, inner_key: str, which:str = 'series', **kwargs) -> DataFrame:
+
+    # initalize out
+    out = DataFrame()
+
+    # grab the column suffix and prefix
+    col_suffix = kwargs.get('col_suffix', None)
+    col_suffix = '' if col_suffix is None else '_' + col_suffix
+    col_prefix = kwargs.get('col_prefix', None)
+    col_prefix = '' if col_prefix is None else col_prefix + '_'
+
+    # tuple comprehend the results based on type
+    if which == "series":
+        out = tuple(v[inner_key].rename(col_prefix + k + col_suffix) for (k, v) in kv.items())
+
+    elif which == "dataframe":
+        out = tuple(v[inner_key].rename(columns={col: col_prefix + '_'.join([k, col]) + col_suffix for col in v[inner_key].columns})
+                    for (k, v) in kv.items())
+
+    out = pd.concat(out, axis=1)
+    out.index.name = kwargs.get('index_name', out.columns.name)
+    out.columns.name = kwargs.get('columns_name', out.columns.name)
+
+    return out
+
+# module classes
 
 class Process(ABC):
 
@@ -100,11 +131,122 @@ class Process(ABC):
         # collect results
         return dict(zip(batch_args.keys(), ray.get(futures)))
 
-    @abstractmethod
+    def compress_results(self, which: Union[list, str] = 'all') -> dict:
+        # check that the process has been run
+        assert self.results_ is not None, r"The %s process has not been run yet!" % (self.__class__.__name__,)
+
+        # grab result keys
+        keys = list(list(self.results_.values())[0].keys())
+
+        # check if items in 'which' are in the keys
+        which = keys if which == 'all' else ([which] if type(which) == str else which)
+        assert set(which) <= set(keys), "Some or all of specified results do not exist in the keys!"
+
+        # initialize output
+        out = dict()
+
+        # run the associated compression methods
+        for key in which:
+            try:
+                result = eval("self._compress_" + key)()
+                out.update({key: result})
+            except AttributeError:
+                warnings.warn("%s does not contain a compression method for %s! Returning \"uncompressed\" object." % (self.__class__.__name__, key))
+                result = self._extract_result(key)
+
+            # update dictionary
+            out.update({key: result})
+
+        return out
+
+    def _extract_result(self, which: str):
+
+        # check that the process has been run
+        assert self.results_ is not None, r"The %s process has not been run yet!" % (self.__class__.__name__,)
+
+        # grab result keys
+        keys = list(list(self.results_.values())[0].keys())
+
+        # check if items in 'which' are in the keys
+        assert which in keys, "%s results does not exist in the keys!" % (which,)
+
+        # initialize output
+        out = dict()
+
+        # extract object from each label
+        for batch in self.results_:
+            out.update({batch: self.results_[batch][which]})
+
+        return out
+
     def save_results(self,
-             save_path: str,
-             overwrite: bool = False):
-        pass
+                     save_path: Union[str, dict] = None,
+                     overwrite: bool = False,
+                     compress: bool = True):
+
+        # make sure there is either a save path or multiple
+        assert save_path is not None, "You must provide a save path or multiple save paths in a dictionary!"
+
+        # make sure the type is correct
+        assert type(save_path) in [str, dict], "save_path must be a string or a dictionary with result keys and string values!"
+
+        # acceptable formats
+        formats = ['.csv', '.pickle']
+
+        if type(save_path) == str:
+            ext = '.' + save_path.split('.')[-1]
+            assert ext == '.pickle', "The file extension must be .pickle for saving the results as a whole!"
+            if compress:
+                save_object(self.compress_results(), save_path, overwrite)
+            else:
+                save_object(self.results_, save_path, overwrite)
+
+        else:
+            assert compress, "Cannot save individual results without compressing the results first, set compress=True!"
+
+            # compress the specificed results
+            results = self.compress_results(which=list(save_path.keys()))
+
+            # loop through each result and save
+            for key in save_path.keys():
+                # grab the specific result
+                result = results[key]
+
+                # check save path
+                ind_save_path = generate_save_path(save_path[key], overwrite)
+
+                # grab extension
+                ext = '.' + ind_save_path.split('.')[-1]
+
+                # check for appropriate extension
+                assert ext in formats, "Saving of file extension %s not yet implemented, use one of the following: %s" \
+                                       % (ext, ', '.join(formats))
+
+                # saved base on extension type
+                if ext == '.csv':
+                    if type(result) == DataFrame or type(result) == Series:
+
+                        # save pandas dataframe/series to .csv
+                        result.to_csv(ind_save_path)
+                        not_implemented_for_ext = False
+                    elif type(result) is ndarray:
+
+                        # save numpy array to .csv
+                        np.savetxt(ind_save_path, result, delimiter=",")
+                        not_implemented_for_ext = False
+                    else:
+                        not_implemented_for_ext = True
+
+                elif ext == '.pickle':
+
+                    # save with pickle
+                    save_object(result, ind_save_path, overwrite)
+                    not_implemented_for_ext = False
+
+                if not_implemented_for_ext:
+                    raise AttributeError("The compressed result %s may not be saved as %s!" % (key, ext))
+
+        return
 
     def save(self,
              save_path: str,
@@ -206,7 +348,7 @@ class Partition(Process):
         # return to run method
         return result
 
-    def run(self, ds:DataSet, batch_args:dict = None, append_labels=True):
+    def run(self, ds:DataSet, batch_args: dict = None, append_labels=True):
 
         # run the super first
         super(Partition, self).run(ds, batch_args)
@@ -243,22 +385,15 @@ class Partition(Process):
 
         return ds, self.results_
 
-    def compress_results(self):
-        assert self.results_ is not None, "The Partition process has not been run yet!"
-        return pd.DataFrame.from_dict({v['tvt_labels'].name :v['tvt_labels'] for (k,v) in self.results_.items()})
+    def _compress_tvt_labels(self):
 
-    def save_results(self,
-                    save_path: str,
-                    overwrite: bool = False):
-
-        # generate new save path in case you don't want to overwrite
-        save_path = generate_save_path(save_path, overwrite)
-
-        # generate dataframe to save labels
-        results = self.compress_results()
-
-        # save labels as .csv
-        results.to_csv(save_path)
+        # compress the dict of dict of series to dataframe
+        results = _compress_dict_dict_pandas(kv=self.results_,
+                                             inner_key="tvt_labels",
+                                             which='series',
+                                             columns_name=self.process_name + " splits",
+                                             col_suffix="split")
+        return results
 
 
 class Fit(Process):
@@ -432,6 +567,123 @@ class Transform(Fit):
         out = {k: v['transform'](ds) for (k, v) in self.results_.items()}
         return out
 
+
+class Feature_Select(Transform):
+
+    def __init__(self,
+                 process: object,
+                 process_name: str = None,
+                 parallel: bool = False,
+                 verbosity: int = 0,
+                 retain_f_ids: bool = False,
+                 fit_handle: str = 'fit',
+                 transform_handle: str = 'transform',
+                 fit_transform_handle: str = 'fit_transform',
+                 supervised_attr: str = None,
+                 fit_args: dict = {}):
+
+
+        # init with Process class
+        super(Transform, self).__init__(process=process,
+                                        process_name=process_name,
+                                        parallel=parallel,
+                                        verbosity=verbosity,
+                                        supervised_attr=supervised_attr,
+                                        fit_handle=fit_handle,
+                                        fit_args=fit_args,
+                                        )
+
+        # set parameters
+        self.retain_f_ids = retain_f_ids
+
+        # set private attributes
+        self._transform_handle = transform_handle
+        self._fit_transform_handle = fit_transform_handle
+
+        # check appropriate parameters
+        if self._fit_handle is None or self._transform_handle is None:
+            if self._fit_transform_handle is None:
+                raise ValueError("Transform process must have either both a fit method and a transform method or just a fit_transform method!")
+            else:
+                warnings.warn("Transform will use its fit_transform method to fit."
+                              " Make sure that its fit_transform method fits"
+                              " the transformation inplace!")
+
+    def _fit_transform(self, ds: DataSet, **kwargs):
+
+        # extract training ids
+        training_ids = self._extract_training_ids(ds, **kwargs)
+
+        #  add supervised labels to fit args
+        if self.supervised_attr is not None:
+            y = ds.metadata.loc[training_ids, self.supervised_attr]
+            self.fit_args['y'] = y
+
+        # fit the process
+        if self.verbosity > 0:
+            print(r"Fitting %s and then transforming data..." % (self.process_name,))
+        process = deepcopy(self.process)
+        data = eval("process." + self._fit_transform_handle)(ds.data.loc[training_ids], **self.fit_args)
+
+        return process, data
+
+    def _run(self, ds:DataSet, **kwargs):
+
+        # initalize output
+        result = dict()
+
+        # run the super's _preprocess method
+        ds_new = self._preprocess(ds, **kwargs)
+
+        # attempt to fit
+        if self._transform_handle is not None:
+            if self._fit_handle is not None:
+                process = self._fit(ds_new, **kwargs)
+            else:
+                process, _ = self._fit_transform(ds_new, **kwargs)
+        else:
+            process = None
+
+        # store resulting transform
+        result['transform'] = self._generate_transform(process)
+
+        return result
+
+    def _generate_transform(self, process=None):
+
+        # define transform
+        def transform(ds: DataSet):
+            if process is None:
+                _, data_new = self._fit_transform(ds)
+            else:
+                if self.verbosity > 0:
+                    print(r"Transforming the data using %s..." % (self.process_name,))
+                data_new = eval("process." + self._transform_handle)(ds.data)
+            if self.retain_f_ids:
+                try:
+                    data_new = pd.DataFrame(data=data_new, index=ds.data.index, columns=ds.data.columns)
+                    ds_new = deepcopy(ds)
+                    ds_new.data = data_new
+                except ValueError:
+                    raise ValueError("Transform changes the dimension of the data and therefore cannot retain the original feature ids in the new dataset!")
+            else:
+                data_new = pd.DataFrame(data=data_new, index=ds.data.index, columns=['_'.join([self.process_name, str(i)]) for i in range(data_new.shape[1])])
+
+                # check if features are the same after transformation and use original feature ids for columns
+                ds_new = DataSet(data=data_new, metadata=ds.metadata)
+
+            return ds_new
+
+        return transform
+
+    def transform(self, ds: DataSet):
+
+        assert self.results_ is not None, "Transform must call its run method on a dataset before it can transform a new dataset!"
+
+        # transform the incoming data according to transforms
+        out = {k: v['transform'](ds) for (k, v) in self.results_.items()}
+        return out
+
     def save_results(self, save_path: str, overwrite: bool = False):
 
         # call save_object on results
@@ -451,6 +703,8 @@ class Classify(Fit):
                  fit_args: dict = {},
                  predict_args: dict = {},
                  classes_handle='classes_',
+                 f_weights_handle: str = None,
+                 s_weights_handle: str = None,
                  ):
 
         # init with Process class
@@ -471,39 +725,58 @@ class Classify(Fit):
         # set private attributes
         self._predict_handle = predict_handle
         self._classes_handle = classes_handle
+        self._f_weights_handle = f_weights_handle
+        self._s_weights_handle = s_weights_handle
 
         # check appropriate parameters
         if self._fit_handle is None or self._predict_handle is None:
             raise ValueError("Classify process must have both a fit method and a predict method!")
-    def compress_results(self):
 
-        assert self.results_ is not None, "The Classify process has not been run yet!"
+    def _compress_class_labels(self):
 
-        result_type = list(list(self.results_.values())[0].keys())[0]
+        # compress the dict of dict of series to dataframe
+        results = _compress_dict_dict_pandas(kv=self.results_,
+                                             inner_key="class_labels",
+                                             which='series',
+                                             columns_name=self.process_name + " labels",
+                                             col_suffix='_'.join([self.process_name, "labels"]))
+        return results
 
-        # tuple comprehend the results based on type
-        if result_type == "class_labels":
-            results = tuple(v[result_type].rename(k) for (k, v) in self.results_.items())
-            results = pd.concat(results, axis=1)
-            results.columns.name = self.process_name + " labels"
-        elif result_type == "class_scores":
-            results = tuple(v[result_type].rename(columns={col: '_'.join([k, col]) for col in v[result_type].columns}) for (k, v) in self.results_.items())
-            results = pd.concat(results, axis=1)
+    def _compress_class_scores(self):
 
-            return results
+        # compress the dict of dict of dataframe to dataframe
+        results = _compress_dict_dict_pandas(kv=self.results_,
+                                             inner_key="class_labels",
+                                             which='dataframe',
+                                             columns_name=self.process_name + " scores",
+                                             col_suffix="scores")
+        return results
 
-    def save_results(self, save_path: str, overwrite: bool = False):
+    def _compress_f_weights(self):
 
-        # check type of result
-        results = self.compress_results()
+        # compress the dict of dict of series to dataframe
+        results = _compress_dict_dict_pandas(kv=self.results_,
+                                             inner_key="f_weights",
+                                             which='series',
+                                             columns_name=self.process_name + " f_weights",
+                                             col_suffix="f_weights")
 
-        # save the results as .csv
-        save_path = generate_save_path(save_path, overwrite)
-        results.to_csv(save_path)
+        return results
+
+    def _compress_s_weights(self):
+
+        # compress the dict of dict of series to dataframe
+        results = _compress_dict_dict_pandas(kv=self.results_,
+                                             inner_key="s_weights",
+                                             which='series',
+                                             columns_name=self.process_name + " s_weights",
+                                             col_suffix="s_weights")
+
+        return results
 
     def _run(self, ds: DataSet, **kwargs):
 
-        # initalize output
+        # initialize output
         result = dict()
 
         # run the super's _preprocess method
@@ -513,12 +786,17 @@ class Classify(Fit):
         process = self._fit(ds_new, **kwargs)
 
         # store resulting transform
-        pred, pred_label = self._generate_labels_or_scores(process, ds_new)
-        result[pred_label] = pred
+        result.update(self._generate_labels_or_scores(process, ds_new))
+
+        # grab potential feature and sample weights
+        result.update(self._generate_f_s_weights(process, ds_new))
+
+        # store the fit process
+        result.update({'classifier': process})
 
         return result
 
-    def _generate_labels_or_scores(self, process: object, ds: DataSet):
+    def _generate_labels_or_scores(self, process: object, ds: DataSet) -> dict:
 
         # predict on dataset
         if self.verbosity > 0:
@@ -535,7 +813,30 @@ class Classify(Fit):
             pred = pd.Series(name=self.process_name + " labels", data=predictions, index=ds.metadata.index)
             pred_label = 'class_labels'
 
-        return pred, pred_label
+        return {pred_label: pred}
 
+    def _generate_f_s_weights(self, process: object, ds: DataSet) -> dict:
+
+        # initialize output
+        out = dict()
+
+        # extract feature weights
+        if self._f_weights_handle is not None:
+            f_weights = pd.Series(index=ds.vardata.index,
+                                  data=eval("process." + self._f_weights_handle),
+                                  name='_'.join([self.process_name, 'f_weights']))
+
+            out.update({'f_weights': f_weights})
+
+        # extract sample weights
+        if self._s_weights_handle is not None:
+            s_weights = pd.Series(index=ds.metadata.index,
+                                  data=eval("process." + self._s_weights_handle),
+                                  name='_'.join([self.process_name, 's_weights']))
+
+            out.update({'s_weights': s_weights})
+
+
+        return out
 
 
