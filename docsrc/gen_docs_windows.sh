31e67cfcf49fd6a98752c90db28a42bf8dd1e85e
ekeho <ekehoe32@gmail.com>
2022-01-19 13:05:21 -0700

    updated docs

--- docsrc/gen_docs_windows.sh
+++ docsrc/gen_docs_windows.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd c:/Users/ekeho/Documents/orthrus/docsrc
-
+source c:/Users/ekeho/anaconda3/etc/profile.d/conda.sh
 conda activate orthrus
 sphinx-apidoc --implicit-namespaces -o rst ../orthrus/
 make github
