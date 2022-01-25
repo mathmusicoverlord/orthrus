b9611255dc2113fc802f3fe6ade7e17e6e22331b
Kartikay Sharma <sharmas.kartik@gmail.com>
2022-01-25 09:15:53 -0700

    Added functionality to specify arguments to fit and transform methods.

--- orthrus/core/pipeline.py
+++ orthrus/core/pipeline.py
@@ -1010,6 +1010,11 @@ class Fit(Process, ABC):
             y = ds.metadata.loc[training_ids, self.supervised_attr]
             self.fit_args['y'] = y
 
+        #  add supervised labels to fit args
+        if self.fit_args.get('group_attr', None) is not None:
+            groups = ds.metadata.loc[training_ids, self.fit_args['group_attr']]
+            self.fit_args['groups'] = groups
+
         # fit the process
         if self.verbosity > 0:
             print(r"Fitting %s..." % (self.process_name,))
@@ -1227,6 +1232,11 @@ class Transform(Fit):
         if self.supervised_attr is not None:
             y = ds.metadata.loc[training_ids, self.supervised_attr]
             self.fit_args['y'] = y
+
+        #  add supervised labels to fit args
+        if self.fit_args['group_attr'] is not None:
+            groups = ds.metadata.loc[training_ids, self.fit_args['group_attr']]
+            self.fit_args['groups'] = groups
 
         # fit the process
         if self.verbosity > 0:
