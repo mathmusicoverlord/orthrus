13c922f7550b11b16dec962ed5fab184c42c9d65
Kartikay Sharma <sharmas.kartik@gmail.com>
2022-01-25 09:16:34 -0700

    Added a partitioning method

--- orthrus/model_selection/partitioning.py
+++ orthrus/model_selection/partitioning.py
@@ -4,6 +4,9 @@ one for each fold in the experiment. See sklearn.model_selection.KFold for an ex
 
 # imports
 import numpy as np
+from sklearn.model_selection import StratifiedGroupKFold
+import copy 
+import pandas as pd
 
 class TrainTestPartitioner():
     def __init__(self, train_idx, test_idx):
@@ -18,3 +21,44 @@ class TrainPartitioner():
         pass
     def split(self, X=None, y=None):
         return [[np.arange(np.shape(X)[0]), []]]
+
+
+class StratifiedGroupKFoldForEachUniqueY():
+        def __init__(self, n_splits, shuffle=False, random_state=42):
+            self.n_splits = n_splits
+            self.shuffle = shuffle
+            self.random_state = random_state
+        
+        def split(self, X, y, groups):
+                train = {}
+                test = {}
+                if isinstance(X, pd.core.frame.DataFrame):
+                    X = X.values
+                if isinstance(y, pd.core.frame.DataFrame):
+                    y = y.values
+                if isinstance(groups, pd.core.frame.DataFrame):
+                    groups = groups.values
+
+                for unique_y in np.unique(y):
+                        idxs_unique_y = np.where(y == unique_y)[0]
+                        cv = StratifiedGroupKFold(n_splits=self.n_splits, shuffle=self.shuffle, random_state=self.random_state)
+                        splits = cv.split(X[idxs_unique_y], y[idxs_unique_y], groups[idxs_unique_y])
+                        for i, split in enumerate(splits):
+                                train_idxs = train.get(i, [])
+                                test_idxs = test.get(i, [])
+                                
+                                train_idxs.extend(idxs_unique_y[split[0]])
+                                test_idxs.extend(idxs_unique_y[split[1]])
+
+                                train[i] = train_idxs
+                                test[i] = test_idxs
+                
+                self.splits = []
+                for k, train_list in train.items():
+                        test_list = test[k]
+                        temp = copy.copy(train_list)
+                        temp.extend(test_list)
+                        assert np.unique(temp).shape[0] == X.shape[0], 'All samples not present in the split'
+                        self.splits.append([train_list, test_list])
+
+                return self.splits
